52d4135a11ec75e4530d0b42d7592bdd49cd1d2d
feldblume5263 <feldblume5263@gmail.com>
2022-06-09 07:57:26 +0900

    feat: init project with templates and ignore file
    project, prTemplate, issueTemplate, commitTemplate, gitignore

---
+++ Techome/Techome/TechomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechomeApp.swift
+//  Techome
+//
+//  Created by Noah Park on 2022/06/09.
+//
+
+import SwiftUI
+
+@main
+struct TechomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
