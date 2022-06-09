20bbfd8869b102d32cbda8cf73a6f11d1cc64b3a
TaekH <103012087+TaekH@users.noreply.github.com>
2022-06-09 10:05:33 +0900

    [feat] #1 ColorSet 추가 color set을 extension으로 추가

---
+++ Techome/Techome/Global/Extension/Color+.swift
@@ -0,0 +1,18 @@
+//
+//  Pallete.swift
+//  Techome
+//
+//  Created by 한택환 on 2022/06/09.
+//
+
+import SwiftUI
+import Foundation
+
+extension Color {
+    static let customBlack = Color("CustomBlack")
+    static let customRed = Color("CustomRed")
+    static let primaryBrown = Color("PrimaryBrown")
+    static let secondaryBrown = Color("SecondaryBrown")
+    static let chartBrown = Color("ChartBrown")
+    static let backgroundCream = Color("BackgroundCream")
+}
