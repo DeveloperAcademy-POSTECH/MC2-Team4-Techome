b8b5ef51f3ab18bdc409c23a3d85366c23b1707e
TaekH <103012087+TaekH@users.noreply.github.com>
2022-06-09 15:06:11 +0900

    [feat] #2 SideEffectRecords 구현
    SideEffectRecord 임시 더미데이터로 구현
    SideEffectRecord 간격 조절 필요

--- Techome/Techome/Views/Trend/TrendView.swift
+++ Techome/Techome/Views/Trend/TrendView.swift
@@ -11,22 +11,22 @@ struct TrendView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-            VStack {
-                TrendChart()
-                SideEffectRecordByDay()
-                CaffeineRecordByDay()
-                    .padding(15)
-                Button{
-                    
-                } label: {
-                    Text("전체 리스트 보기")
-                        .font(.system(size: 15))
-                        .foregroundColor(.secondaryTextGray)
+                VStack(spacing: 0) {
+                    TrendChart()
+                    SideEffectRecordsByDay()
+                    CaffeineRecordByDay()
+                        .padding(15)
+                    Button{
+                        
+                    } label: {
+                        Text("전체 리스트 보기")
+                            .font(.system(size: 15))
+                            .foregroundColor(.secondaryTextGray)
+                    }
+                    Spacer()
                 }
-                Spacer()
+                .navigationTitle("카페인 리포트")
             }
-            .navigationTitle("카페인 리포트")
-        }
         }
     }
 }
@@ -35,49 +35,75 @@ struct TrendChart: View {
     let chartWidth: CGFloat = UIScreen.main.bounds.width - 30
     
     var body: some View {
-            LazyHStack() {
-                TabView {
-                    ForEach(0..<5) {index in
-                        VStack(alignment: .leading, spacing: 0) {
-                            Group {
-                                Text("하루 평균 섭취량")
-                                    .font(.subheadline)
-                                    .foregroundColor(.gray)
-                                    .padding(.bottom, 5)
-                                    .padding(.top, 15)
-                                HStack(alignment: .firstTextBaseline, spacing: 0){
-                                    Text("360")
-                                        .font(.largeTitle)
-                                        .foregroundColor(.black)
-                                    Text("mg")
-                                        .font(.body)
-                                        .foregroundColor(.secondaryTextGray)
-                                }
+        LazyHStack() {
+            TabView {
+                ForEach(0..<5) {index in
+                    VStack(alignment: .leading, spacing: 0) {
+                        Group {
+                            Text("하루 평균 섭취량")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
                                 .padding(.bottom, 5)
-                                Text("2022.06.03~2022.06.09")
+                                .padding(.top, 15)
+                            HStack(alignment: .firstTextBaseline, spacing: 0){
+                                Text("360")
+                                    .font(.largeTitle)
+                                    .foregroundColor(.black)
+                                Text("mg")
                                     .font(.body)
                                     .foregroundColor(.secondaryTextGray)
                             }
-                            .padding(.leading, 15)
-                            Spacer()
+                            .padding(.bottom, 5)
+                            Text("2022.06.03~2022.06.09")
+                                .font(.body)
+                                .foregroundColor(.secondaryTextGray)
                         }
-                        .frame(maxWidth: chartWidth, alignment: .leading)
-                        .background(RoundedRectangle(cornerRadius: 5)
-                            .foregroundColor(.white)
-                            .shadow(color: .secondaryTextGray, radius: 5, x: 0, y: 0))
-                        .padding(15)
+                        .padding(.leading, 15)
+                        Spacer()
                     }
+                    .frame(maxWidth: chartWidth, alignment: .leading)
+                    .background(RoundedRectangle(cornerRadius: 5)
+                        .foregroundColor(.white)
+                        .shadow(color: .secondaryTextGray, radius: 2, x: 0, y: 0))
+                    .padding(15)
                 }
-                .frame(width: UIScreen.main.bounds.width, height: chartWidth * 1.26, alignment: .center)
-                .tabViewStyle(.page(indexDisplayMode: .never))
             }
+            .frame(width: UIScreen.main.bounds.width, height: chartWidth * 1.26, alignment: .center)
+            .tabViewStyle(.page(indexDisplayMode: .never))
+        }
         .frame(height: chartWidth * 1.26)
     }
 }
 
-struct SideEffectRecordByDay: View {
+struct SideEffectRecordsByDay: View {
+    let chartWidth: CGFloat = UIScreen.main.bounds.width - 30
+    let columns = [
+        GridItem(.flexible(maximum: 80)),
+        GridItem(.flexible(maximum: 80))
+    ]
+    
     var body: some View {
-        Text("SideEffect")
+        
+        LazyHGrid(rows: columns, spacing: 32) {
+            ForEach(0..<10) { idx in
+                SideEffectRecord()
+                    .padding(.vertical, 14)
+            }
+        }
+        .frame(width: chartWidth, height: chartWidth / 2.4)
+        .background(RoundedRectangle(cornerRadius: 7)
+            .foregroundColor(.white)
+            .shadow(color: .secondaryTextGray, radius: 2, x: 0, y: 0))
+    }
+}
+
+struct SideEffectRecord: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("esophagitis")
+            Text("식도염")
+                .font(.subheadline)
+        }
     }
 }
 
@@ -97,30 +123,30 @@ struct CaffeineRecordByDay: View {
 struct CaffeineRecordRow: View {
     var body: some View {
         VStack(spacing: 0) {
-        HStack(alignment: .center, spacing: 0) {
-            VStack(alignment: .leading){
-                Text("09:30")
-                    .font(.caption)
-                    .foregroundColor(.secondaryTextGray)
-                HStack(alignment: .firstTextBaseline, spacing: 0){
-                    Text("아메리카노")
-                        .font(.title3)
-                    Text("스타벅스/Tall")
+            HStack(alignment: .center, spacing: 0) {
+                VStack(alignment: .leading){
+                    Text("09:30")
                         .font(.caption)
+                        .foregroundColor(.secondaryTextGray)
+                    HStack(alignment: .firstTextBaseline, spacing: 0){
+                        Text("아메리카노")
+                            .font(.title3)
+                        Text("스타벅스/Tall")
+                            .font(.caption)
+                            .foregroundColor(.secondaryTextGray)
+                    }
+                }
+                Spacer()
+                HStack (alignment: .firstTextBaseline, spacing: 0){
+                    Text("150")
+                        .font(.title)
+                    Text("mg")
+                        .font(.subheadline)
                         .foregroundColor(.secondaryTextGray)
                 }
             }
-            Spacer()
-            HStack (alignment: .firstTextBaseline, spacing: 0){
-            Text("150")
-                .font(.title)
-            Text("mg")
-                .font(.subheadline)
-                .foregroundColor(.secondaryTextGray)
-            }
-        }
-        .padding(15)
-        Divider()
+            .padding(15)
+            Divider()
             
         }
     }
