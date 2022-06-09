a60b2cf577e37fac03bf2f3c92e3d0eff8dd4995
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-09 10:28:48 +0900

    [add] SearchCaffeineView 생성

---
+++ Techome/Techome/Views/AddCaffeine/SearchCaffeineView.swift
@@ -0,0 +1,20 @@
+//
+//  addCaffeine.swift
+//  Techome
+//
+//  Created by Seungyun Kim on 2022/06/09.
+//
+
+import SwiftUI
+
+struct SearchCaffeineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchCaffeineView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchCaffeineView()
+    }
+}
