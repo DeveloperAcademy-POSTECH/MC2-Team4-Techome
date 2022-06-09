d2b6e250b0e952dd2f9feea5c0877573964d586d
PodingDA <yw1221@g.skku.edu>
2022-06-09 13:47:47 +0900

    [feat] #3 데이터 모델 생성 완료
    카페인음료, 카페인부작용 데이터베이스 생성 완료.
    Json manager save, load 완료.

---
+++ Techome/Techome/Model/Drink/Franchise.swift
@@ -0,0 +1,46 @@
+//
+//  Franchise.swift
+//  Techome
+//
+//  Created by Yeongwoo Kim on 2022/06/09.
+//
+
+import Foundation
+
+enum Franchise: String{
+    case starbucks = "스타벅스"
+    case twosomePlace = "투썸플레이스"
+    
+    func getCaffeinPerShot() -> Int{
+        switch(self){
+        case .starbucks:
+            return 75
+        case .twosomePlace:
+            return 50                   //멀랑...
+        }
+    }
+    
+    func getSizeName(index: Int) -> String{
+        switch(self){
+        case .starbucks:
+            let names = ["Tall", "Grande", "Venti"]
+            return names[index]
+            
+        case .twosomePlace:
+            let names = ["Regular", "Large", "Max"]
+            return names[index]
+        }
+    }
+    
+    func getSizeAmount(index: Int) -> Int{
+        switch(self){
+        case .starbucks:
+            let names = [355, 473, 591]
+            return names[index]
+            
+        case .twosomePlace:
+            let names = [355, 473, 591]
+            return names[index]
+        }
+    }
+}
