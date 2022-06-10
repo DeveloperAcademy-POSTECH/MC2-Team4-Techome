020b374f148f548123c45fe5d57590468d4a89e8
hwiwonK <gnldnjs2469@gmail.com>
2022-06-10 11:54:12 +0900

    [feat] Total List 파일 생성
    파일 생성

---
+++ Techome/Techome/Views/TotalList/TotalListView.swift
@@ -0,0 +1,20 @@
+//
+//  TotalListView.swift
+//  Techome
+//
+//  Created by Kimhwiwon on 2022/06/10.
+//
+
+import SwiftUI
+
+struct TotalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TotalListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TotalListView()
+    }
+}
