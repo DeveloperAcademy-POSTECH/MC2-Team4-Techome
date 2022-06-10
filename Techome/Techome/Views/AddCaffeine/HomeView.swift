933a52c2b74f4eef2e6006f17f372ef662fa3909
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-10 14:46:56 +0900

    [add] 카페인 추가하기 풀스크린오버용 임시 Homeview 생성

--- Techome/Techome/Views/AddCaffeine/HomeView.swift
+++ Techome/Techome/Views/AddCaffeine/HomeView.swift
@@ -1,23 +1,20 @@
 //
-//  addCaffeine.swift
+//  HomeView.swift
 //  Techome
 //
-//  Created by Seungyun Kim on 2022/06/09.
+//  Created by Seungyun Kim on 2022/06/10.
 //
 
 import SwiftUI
 
-struct SearchCaffeineView: View {
+struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-
-
-
-struct SearchCaffeineView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        SearchCaffeineView()
+        HomeView()
     }
 }
