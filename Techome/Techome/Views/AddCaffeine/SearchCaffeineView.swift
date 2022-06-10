81d1604f4f30fa440f78dda656eaaefb121936c7
Nick(Seungyun Kim) <sy5072@naver.com>
2022-06-10 17:29:53 +0900

    [feat] 카페인 추가하기 UI 구현 #7
    검색바는 임시로 사각형
    최근 추가한 리스트는 더미데이터로 구성

--- Techome/Techome/Views/AddCaffeine/SearchCaffeineView.swift
+++ Techome/Techome/Views/AddCaffeine/SearchCaffeineView.swift
@@ -10,27 +10,87 @@ import SwiftUI
 struct SearchCaffeineView: View {
     @Environment(\.presentationMode) var presentationMode
     let widthPadding = 15.0
+    let listTextPadding = 10.0
     
     var body: some View {
-        ZStack(alignment: .top) {
-            Color(.white)
-                .ignoresSafeArea()
+        NavigationView {
+            ZStack {
+                Color.backgroundCream
+                    .ignoresSafeArea()
             
-            
-            Text("카페인 추가하기")
-                .fontWeight(.bold)
-                .foregroundColor(.black)
-                .frame(maxWidth:.infinity, alignment: .center)
-                .padding(EdgeInsets(top: widthPadding, leading: 0, bottom: 0, trailing: 0))
-            
-            Button(action: {
-                presentationMode.wrappedValue.dismiss()
-            }) {
-                Text("취소")
-                    .foregroundColor(.primaryBrown)
-                    .padding(EdgeInsets(top: widthPadding, leading: 0, bottom: 0, trailing: widthPadding))
+                VStack {
+                    RoundedRectangle(cornerRadius: 5)
+                        .frame(height: 48)
+                        .foregroundColor(.white)
+                        .shadow(color: .secondaryTextGray, radius: 2, x: 0, y: 0)
+                    
+                    
+                    Text("최근 추가한 카페인")
+                        .foregroundColor(.black)
+                        .fontWeight(.bold)
+                        .frame(maxWidth:.infinity, alignment: .leading)
+                        .padding(EdgeInsets(top: widthPadding, leading: listTextPadding, bottom: 0, trailing: 0))
+                    
+                    
+                    CaffeineRecordByDay()
+                }
+                .frame(height: UIScreen.main.bounds.height*0.8, alignment: .top)
+                .padding(.horizontal)
             }
-            .frame(maxWidth:.infinity, alignment: .trailing)
+            .navigationTitle("카페인 추가하기")ㅊ
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("취소")
+                            .foregroundColor(.primaryBrown)
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+struct CaffeineRecordByDay: View {
+    var body: some View {
+        LazyVStack(spacing: 0) {
+            ForEach (0 ..< 7) {_ in
+                CaffeineRecordRow()
+            }
+        }
+        .background(RoundedRectangle(cornerRadius: 7)
+            .foregroundColor(.white)
+            .shadow(color: .secondaryTextGray, radius: 2, x: 0, y: 0))
+    }
+}
+
+struct CaffeineRecordRow: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(alignment: .center, spacing: 0) {
+                VStack (alignment: .leading){
+                    Text("아메리카노")
+                        .font(.title3)
+                    Text("스타벅스/Tall")
+                        .font(.caption)
+                        .foregroundColor(.secondaryTextGray)
+                }
+                Spacer()
+                HStack (alignment: .firstTextBaseline, spacing: 0){
+                    Text("150")
+                        .font(.title3)
+                    Text("mg")
+                        .font(.subheadline)
+                        .foregroundColor(.secondaryTextGray)
+                }
+            }
+            .foregroundColor(.black)
+            .padding(15)
+            Divider()
+            
         }
     }
 }
