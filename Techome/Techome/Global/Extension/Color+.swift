f35b2caa2235e5ecaf1ac63760203b53d73276f2
feldblume5263 <feldblume5263@gmail.com>
2022-06-13 15:00:13 +0900

    [feat] 컬러셋, 투데이뷰 게이지 이미지 추가
    투데이뷰 게이지 백그라운드 이미지 추가.
    컬러셋 GaugeBackgroundGray 추가.

--- Techome/Techome/Global/Extension/Color+.swift
+++ Techome/Techome/Global/Extension/Color+.swift
@@ -20,4 +20,5 @@ extension Color {
     static let secondaryShadowGray = Color("SecondaryShadowGray")
     static let chartBackgroundLineGray = Color("ChartBackgroundLineGray")
     static let chartIndicatorBackgroundGray = Color("ChartIndicatorBackgroundGray")
+    static let gaugeBackgroundGray = Color("GaugeBackgroundGray")
 }
