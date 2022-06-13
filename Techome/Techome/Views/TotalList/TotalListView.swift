7deab357b8dc2c56783b0b32ef6dfcb0b116ced3
hwiwonK <gnldnjs2469@gmail.com>
2022-06-13 18:52:36 +0900

    [feat] #6 Total List 피드백 반영
    - private 접근 제한자 추가
    - 사용하지 않는 변수 제거, 변수 선언 위치 변경, 변수명 변경
    - 불필요한 개행 제거
    - if문 -> switch문으로 구현 방식 변경
    - 주석 수정

--- Techome/Techome/Views/TotalList/TotalListView.swift
+++ Techome/Techome/Views/TotalList/TotalListView.swift
@@ -11,7 +11,6 @@ import SwiftUI
 struct TotalListLayoutValue {
     
     struct Paddings {
-        
         static let dayRecordPadding: CGFloat = 4
         static let caffeineRecordRowVerticalPadding: CGFloat = 15
         static let caffeineRecordRowHorizontalPadding: CGFloat = 20
@@ -25,32 +24,21 @@ struct TotalListLayoutValue {
         static let fullViewVerticalPadding: CGFloat = 23
         static let dateVerticalPadding: CGFloat = 6
         static let dividerHorizontalPadding: CGFloat = 15
-        
     }
     
     struct Sizes {
-        static let mainWidth: CGFloat = UIScreen.main.bounds.width
-        static let mainHeight: CGFloat = UIScreen.main.bounds.height
         static let cardWidth: CGFloat = UIScreen.main.bounds.width - 30
         static let sideEffectRecordCellFixedWidth: CGFloat = 46
     }
-    
-    
 }
+
 
 //카페인 + 부작용 리스트 병합에 사용
-struct tmpDataTotalList : Hashable {
+struct TmpDataTotalList : Hashable {
     var dataType: String
-    var dataIdx: Int
+    var dataIndex: Int
 }
 
-//카페인 + 부작용 리스트 병합 결과
-//(추후 구현) datatype과 idx 활용해 리스트에 넣을 알맞은 데이터 찾기
-private var tmpDataTotalListArr : [tmpDataTotalList] = [
-    tmpDataTotalList(dataType: "drink", dataIdx: 1),
-    tmpDataTotalList(dataType: "sideEffect", dataIdx: 1),
-    tmpDataTotalList(dataType: "drink", dataIdx: 2)
-]
 
 // 전체 리스트
 struct TotalListView: View {
@@ -58,15 +46,11 @@ struct TotalListView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        
         ScrollView(){
-        
             LazyVStack(spacing: 23){
                 ForEach(1..<10) { _ in
                     TotalRecordsByDay(curDate: "2022.06.03")
                 }
-                
-                
             }
             .padding(.horizontal, TotalListLayoutValue.Paddings.fullViewHorizontalPadding)
             .padding(.top, TotalListLayoutValue.Paddings.fullViewVerticalPadding) //navigation bar와 간격
@@ -89,67 +73,66 @@ struct TotalListView: View {
     }
 }
 
+
 //날짜별 카페인 + 부작용 데이터 : 날짜별로 스트레스 + 부작용 데이터 받아와서 뷰 만들기
-//(추후) 카페인 데이터인지 부작용 데이터인지에 따라 맞는 리스트 보여주도록 구현
 struct TotalRecordsByDay: View {
-    let curDate : String
-    let dataCount = tmpDataTotalListArr.count
+    //카페인 + 부작용 리스트 병합 결과
+    //TODO: datatype과 idx 활용해 리스트에 넣을 알맞은 데이터 찾기
+    private let TmpDataTotalListArr : [TmpDataTotalList] = [
+        TmpDataTotalList(dataType: "drink", dataIndex: 1),
+        TmpDataTotalList(dataType: "sideEffect", dataIndex: 1),
+        TmpDataTotalList(dataType: "drink", dataIndex: 2)
+    ]
+    
+    private let curDate : String
+    private let dataCount : Int
+    
+    init(curDate: String){
+        self.curDate = curDate
+        self.dataCount = TmpDataTotalListArr.count
+    }
     
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 0){
-            
             Text(curDate)
                 .font(.title3)
                 .fontWeight(.semibold)
                 .padding(.bottom, TotalListLayoutValue.Paddings.dateVerticalPadding)
         
             VStack(spacing: 0){
-                
-                ForEach(Array(tmpDataTotalListArr.enumerated()), id: \.element) { idx, element in
-                    if (element.dataType == "drink"){
+                ForEach(Array(TmpDataTotalListArr.enumerated()), id: \.element) { index, element in
+                    switch element.dataType {
+                    case "drink":
                         CaffeineRecordCellList()
                             .padding(.horizontal, TotalListLayoutValue.Paddings.caffeineRecordRowHorizontalPadding)
-                    }
-                    else if (element.dataType == "sideEffect"){
+                    case "sideEffect":
                         SideEffectRecordRow()
-                    }
-                    else {
                         
+                    default :
+                        EmptyView()
                     }
                     
                     //마지막 데이터 다음 divider 없애기
-                    if (idx != tmpDataTotalListArr.count - 1) {
+                    if (index != dataCount - 1) {
                         Divider()
                             .padding(.horizontal, TotalListLayoutValue.Paddings.dividerHorizontalPadding)
                     }
-                    
                 }
-
             }
             .padding(.vertical, 3)
             .background(Color.white)
             .clipShape(RoundedRectangle(cornerRadius: 7))
             .shadow(color: Color.primaryShadowGray, radius: 7, x: 0, y: 0)
-            
         }
     }
-    
 }
 
 
-
-//부작용 데이터
-
-//부작용 시간 + 부작용 정보
+//부작용 데이터 컴포넌트 : 부작용 시간 + 부작용 정보
 struct SideEffectRecordRow: View {
-    
     var body: some View {
-        
         VStack(alignment: .leading, spacing: 0) {
-            
             HStack(spacing: 0){
-            
                 Text("09:30")
                     .font(.caption)
                     .foregroundColor(.secondaryTextGray)
@@ -162,11 +145,9 @@ struct SideEffectRecordRow: View {
             .padding(.leading, TotalListLayoutValue.Paddings.sideEffectRecordRowHorizontalPadding)
             
             SideEffectRecordsByDayList()
-            
         }
         .padding(.top, TotalListLayoutValue.Paddings.sideEffectRecordRowVerticalPadding)
     }
-    
 }
 
 //============================================================================
@@ -174,8 +155,7 @@ struct SideEffectRecordRow: View {
 
 
 //trend 의 sideEffectRecordsByDay 와 동일
-//부작용 데이터
-
+//부작용 데이터 컴포넌트 : 부작용 정보
 struct SideEffectRecordsByDayList: View {
     var body: some View {
         VStack(alignment: .center, spacing: TotalListLayoutValue.Paddings.sideEffectRecordCellVerticalPadding) {
@@ -193,7 +173,9 @@ struct SideEffectRecordsByDayList: View {
     }
 }
 
+
 //trend 의 SideEffectRecordItem 과 동일
+//부작용 데이터 컴포넌트 : 부작용 이미지 + 이름
 struct SideEffectRecordItemList: View {
     var body: some View {
         VStack(spacing: 0) {
@@ -204,7 +186,6 @@ struct SideEffectRecordItemList: View {
         .frame(width: TotalListLayoutValue.Sizes.sideEffectRecordCellFixedWidth)
     }
 }
-
 
 
 //trend 의 CaffeineRecordCell 에 padding (.vertical)로 제한하는 코드 추가 & Divider 삭제, 이외 동일
@@ -243,7 +224,6 @@ struct CaffeineRecordCellList: View {
         }
     }
 }
-
 
 
 struct TotalListView_Previews: PreviewProvider {
