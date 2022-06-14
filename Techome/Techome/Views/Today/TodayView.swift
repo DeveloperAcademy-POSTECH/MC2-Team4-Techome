a815353e87408a06ae2e6dfffe0b5729207c69d1
feldblume5263 <feldblume5263@gmail.com>
2022-06-14 16:57:59 +0900

    [feat] #30 update buttons component
    button 업로드

--- Techome/Techome/Views/Today/TodayView.swift
+++ Techome/Techome/Views/Today/TodayView.swift
@@ -9,31 +9,29 @@ import SwiftUI
 import SineWaveShape
 
 struct MainView: View {
-
+    
     init() {
         UITabBar.appearance().backgroundColor = .white
     }
     
     var body: some View {
-        NavigationView {
-            TabView {
-                TodayView()
-                    .navigationBarHidden(true)
-                    .tabItem {
-                        Image(systemName: "1.square.fill")
-                        Text("투데이")
-                    }
-                Text("카페인북 view가 여기 들어와야 해요.")
-                    .tabItem {
-                        Image(systemName: "2.square.fill")
-                        Text("카페인북")
-                    }
-                Text("리포트 view가 여기 들어와야 해요.")
-                    .tabItem {
-                        Image(systemName: "3.square.fill")
-                        Text("리포트")
-                    }
-            }
+        TabView {
+            TodayView()
+                .navigationBarHidden(true)
+                .tabItem {
+                    Image(systemName: "1.square.fill")
+                    Text("투데이")
+                }
+            Text("카페인북 view가 여기 들어와야 해요.")
+                .tabItem {
+                    Image(systemName: "2.square.fill")
+                    Text("카페인북")
+                }
+            Text("리포트 view가 여기 들어와야 해요.")
+                .tabItem {
+                    Image(systemName: "3.square.fill")
+                    Text("리포트")
+                }
         }
     }
 }
@@ -48,10 +46,25 @@ struct TodayView: View {
                 BackgroundAnimationView(caffeinePercent: $caffeinePercent)
                 VStack(spacing: 0) {
                     TodayViewTopBottons(caffeinePercent: $caffeinePercent, geometry: geometry)
-                    Spacer()
+                        .padding(.top, 9)
+                        .padding(.trailing, 30)
+                        .padding(.bottom, 25)
+                    RemainingCaffeinStatement()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 30)
+                        .padding(.bottom, 374)
+                    ExhaustTimeStatement()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 30)
+                        .padding(.bottom, 40)
+                    AddCaffeineButton()
+                        .frame(maxWidth: .infinity, alignment: .trailing)
+                        .padding(.trailing, 30)
+                        .padding(.bottom, 100)
                 }
             }
-            .ignoresSafeArea(.all)
+            .ignoresSafeArea()
+            
         }
     }
 }
@@ -67,13 +80,13 @@ struct BackgroundAnimationView: View {
         ZStack {
             Color.gaugeBackgroundGray
             SineWaveShape(percent: caffeinePercent, strength: 30, frequency: 7, phase: self.phase)
-                .fill(Color.primaryBrown)
-                                    .offset(y: CGFloat(1) * 2)
-                                    .animation(.linear(duration: 3).repeatForever(autoreverses: false), value: phase)
+                .fill(Color.secondaryBrown)
+                .offset(y: CGFloat(1) * 2)
+                .animation(.linear(duration: 3).repeatForever(autoreverses: false), value: phase)
             Image("TodayGauge")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
-                .frame(width: screenSize.width)
+                .frame(height: screenSize.height)
         }
         .onAppear {
             phase = .pi * 2
@@ -90,7 +103,7 @@ struct TodayViewTopBottons: View {
             Spacer()
             Button {
                 print("add sideEffect button pressed")
-                caffeinePercent -= 0.1 // 애니메이션 Test를 위한 임시 로직
+                caffeinePercent -= 0.1 // 애니메이션 Test를 위한 임시 로직)
             } label: {
                 Image("AddSideEffectViewIcon")
                     .resizable()
@@ -111,12 +124,70 @@ struct TodayViewTopBottons: View {
                     .foregroundColor(.primaryBrown)
             }
         }
-        .padding(.top, 9)
-        .padding(.trailing, 30)
-        
     }
 }
 
+struct RemainingCaffeinStatement: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(alignment: .firstTextBaseline, spacing: 0) {
+                Text("몸속에")
+                    .font(.title3)
+                    .padding(.trailing, 8)
+                Text("137")
+                    .font(.system(size: 53).bold())
+                    .foregroundColor(.primaryBrown)
+                Text("mg의")
+                    .font(.title3)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            Text("카페인이 남아있고")
+                .font(.title3)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+    }
+}
+
+struct ExhaustTimeStatement: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            HStack(alignment: .firstTextBaseline, spacing: 0) {
+                Text("오후 9시 15분")
+                    .font(.system(size: 30).bold())
+                    .foregroundColor(.primaryBrown)
+                Text("에")
+                    .font(.title3)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            Text("모두 배출되요")
+                .font(.title3)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+    }
+}
+
+struct AddCaffeineButton: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Button {
+                // TODO: modal addCaffeine view
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 7)
+                        .aspectRatio(CGSize(width: 4.0, height: 1.0), contentMode: .fit)
+                        .foregroundColor(.primaryBrown)
+                    Text("카페인 추가하기")
+                        .font(.body.bold())
+                        .foregroundColor(.white)
+                }
+                
+            }
+            .padding(.bottom, 10)
+            Text("오늘 총 300mg 섭취")
+                .font(.caption)
+        }
+    }
+}
 
 
 struct TodayView_Previews: PreviewProvider {
