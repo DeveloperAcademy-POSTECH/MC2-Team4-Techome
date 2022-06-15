b850e06f5ef969bd080b5c2f9cd5edff56e5a8fc
Guel-git <eccyuna@gmail.com>
2022-06-16 00:40:43 +0900

    [feat] #48 SettingsView UI 구현 -TODO: 코드 모듈화 예정

--- Techome/Techome/Views/Settings/SettingsView.swift
+++ Techome/Techome/Views/Settings/SettingsView.swift
@@ -8,8 +8,173 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    @Environment(\.presentationMode) var mode: Binding<PresentationMode>
+    
+    @State private var recordNotice: Bool = true
+    @State private var warningNotice: Bool = true
+    @State private var trendNotice: Bool = true
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+            ZStack(alignment: .leading) {
+                Color.backgroundCream.edgesIgnoringSafeArea(.all)
+                
+                VStack(alignment: .leading, spacing: .zero) {
+                    Text("알림")
+                        .padding(.leading, 18)
+                        .padding(.top, 38)
+                        .font(.title3)
+                        .foregroundColor(.customBlack)
+                    
+                    VStack(spacing: .zero) {
+
+                        Toggle("기록 알림", isOn: $recordNotice)
+                            .padding(.horizontal, 17)
+                            .padding(.vertical, 11)
+                            
+
+                        Divider()
+                            .foregroundColor(.primaryShadowGray)
+                            .padding(.horizontal, 8)
+                        
+                    
+                        Toggle("경고 알림", isOn: $warningNotice)
+                            .frame(width: 326, height: 31)
+                            .padding(.horizontal, 17)
+                            .padding(.vertical, 11)
+                        
+                        Divider()
+                            .foregroundColor(.primaryShadowGray)
+                            .padding(.horizontal, 8)
+                       
+                        Toggle("추이 알림", isOn: $trendNotice)
+                            .padding(.horizontal, 17)
+                            .padding(.vertical, 11)
+                        
+                    }
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+                    .padding(.horizontal, 15)
+                    .padding(.top, 8)
+                    .shadow(color: Color.primaryShadowGray, radius: 4, x: .zero, y: .zero)
+                    .font(.body)
+                    .foregroundColor(.customBlack)
+                    .tint(.primaryBrown)
+                    
+                    Text("정보")
+                        .padding(.leading, 18)
+                        .padding(.top, 46)
+                        .font(.title3)
+                        .foregroundColor(.customBlack)
+                    
+                    VStack(spacing: .zero) {
+                        NavigationLink(destination: {
+                            SettingsCompanyView()
+                                .navigationBarHidden(true)
+                        }){
+                        HStack {
+                            Text("회사 소개")
+                                .font(.body)
+                                .foregroundColor(.customBlack)
+                            Spacer()
+                            Image(systemName: "greaterthan")
+                                .foregroundColor(.customBlack)
+                        }
+                        .padding(.horizontal, 17)
+                        .padding(.vertical, 16)
+
+                        }
+
+                        Divider()
+                            .foregroundColor(.primaryShadowGray)
+                            .padding(.horizontal, 8)
+                        
+                        NavigationLink(destination: {
+                            SettingsOpenSourceView()
+                                .navigationBarHidden(true)
+                        }){
+                        HStack {
+                            Text("오픈소스 라이브러리")
+                                .font(.body)
+                                .foregroundColor(.customBlack)
+                            Spacer()
+                            Image(systemName: "greaterthan")
+                                .foregroundColor(.customBlack)
+                        }
+                        .padding(.horizontal, 17)
+                        .padding(.vertical, 16)
+                        }
+                        
+                        Divider()
+                            .foregroundColor(.primaryShadowGray)
+                            .padding(.horizontal, 8)
+                        
+                        NavigationLink(destination: {
+                            SettingsPersonalInformationView()
+                                .navigationBarHidden(true)
+                        }){
+                        HStack {
+                            Text("개인정보 및 보안 도움말")
+                                .font(.body)
+                                .foregroundColor(.customBlack)
+                            Spacer()
+                            Image(systemName: "greaterthan")
+                                .foregroundColor(.customBlack)
+                        }
+                        .padding(.horizontal, 17)
+                        .padding(.vertical, 16)
+                        }
+                        
+                        Divider()
+                            .foregroundColor(.primaryShadowGray)
+                            .padding(.horizontal, 8)
+                        
+                        NavigationLink(destination: {
+                            SettingsAddOpinionView()
+                                .navigationBarHidden(true)
+                        }){
+                        HStack {
+                            Text("개발자에게 의견 남기기")
+                                .font(.body)
+                                .foregroundColor(.customBlack)
+                            Spacer()
+                            Image(systemName: "greaterthan")
+                                .foregroundColor(.customBlack)
+                                .font(.body)
+                        }
+                        .padding(.horizontal, 17)
+                        .padding(.vertical, 16)
+                        }
+                        
+                    }
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 7))
+                    .padding(.horizontal, 15)
+                    .padding(.top, 8)
+                    .shadow(color: Color.primaryShadowGray, radius: 4, x: .zero, y: .zero)
+                    .font(.body)
+                    .foregroundColor(.customBlack)
+                    
+                    Spacer()
+                }
+
+            }
+            .navigationTitle("설정")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(
+                leading: Button(action: {
+                    self.mode.wrappedValue.dismiss()
+                }) {
+                    Image(systemName: "multiply")
+                        .font(.headline)
+                        .foregroundColor(.primaryBrown)
+                }
+            )
+        }
+        
     }
 }
 
