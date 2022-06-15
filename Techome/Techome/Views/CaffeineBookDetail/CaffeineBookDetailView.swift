d2f6130508a4c0f1081117b004ed839b128a9e58
Guel-git <eccyuna@gmail.com>
2022-06-15 10:09:01 +0900

    [feat] #36 layout 요소 정의 - CaffeineBookDetailLayoutValue struct 선언 - padding, size, radius 값 정의

--- Techome/Techome/Views/CaffeineBookDetail/CaffeineBookDetailView.swift
+++ Techome/Techome/Views/CaffeineBookDetail/CaffeineBookDetailView.swift
@@ -7,6 +7,38 @@
 
 import SwiftUI
 
+struct CaffeineBookDetailLayoutValue {
+    
+    struct Padding {
+        static let backgroundTop: CGFloat = 15
+        static let backgroundHorizantal: CGFloat = 15
+        static let titleTop: CGFloat = 43
+        static let franchiseTop: CGFloat = 8
+        static let buttonGroupTop: CGFloat = 38
+        static let buttonGroupBottom: CGFloat = 6
+        static let buttonBetweenSpace: CGFloat = 21
+        static let buttonTextSpace: CGFloat = 12
+        static let infoRowBetweenSpaceExceptCaption: CGFloat = 21
+        static let infoRowBetweenSpaceCaption: CGFloat = 19
+        static let infoRowBottom: CGFloat = 38
+        static let infoGroupHorizontal: CGFloat = 19
+        static let recommendedAmountTop: CGFloat = 5
+    }
+    
+    struct Size {
+        static let sizeButtonWidth: CGFloat = 92
+        static let sizeButtonHeight: CGFloat = 98
+    }
+    
+    struct Radius {
+        static let backgroundCard: CGFloat = 10
+        static let backgroundCardShadow: CGFloat = 4
+        static let sizeButton: CGFloat = 5
+        static let sizeButtonShadow: CGFloat = 4
+    }
+    
+}
+
 struct CaffeineBookDetailView: View {
     var body: some View {
         NavigationView {
@@ -15,124 +47,15 @@ struct CaffeineBookDetailView: View {
                 
                 VStack {
                     VStack(alignment: .center, spacing: .zero) {
-                        Text("아메리카노")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .foregroundColor(.customBlack)
-                            .padding(.top, 43)
-                        
-                        Text("스타벅스")
-                            .font(.caption)
-                            .foregroundColor(.secondaryTextGray)
-                            .padding(.top, 8)
-                        
-                        HStack(spacing: 21) {
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 92, height: 98)
-                                .foregroundColor(.primaryBrown)
-                                .shadow(color: Color.secondaryShadowGray, radius: 4, x: 0, y: 0)
-                                .overlay(
-                                    VStack(spacing: 0) {
-                                        Text("Tall")
-                                            .font(.title2)
-                                        Text("355ml")
-                                            .padding(.top, 12)
-                                            .font(.caption)
-                                            .foregroundColor(.secondaryTextGray)
-                                    }
-                                        .foregroundColor(.white)
-                                )
-                                .onTapGesture {
-                                    print("Tall 누름")
-                                }
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 92, height: 98)
-                                .foregroundColor(.white)
-                                .shadow(color: Color.primaryShadowGray, radius: 2, x: 0, y: 0)
-                                .overlay(
-                                    VStack(spacing: 0) {
-                                        Text("Tall")
-                                            .font(.title2)
-                                        Text("355ml")
-                                            .padding(.top, 12)
-                                            .font(.caption)
-                                            .foregroundColor(.secondaryTextGray)
-                                    })
-                                .foregroundColor(.customBlack)
-                                .onTapGesture {
-                                    print("Tall 누름")
-                                }
-                            RoundedRectangle(cornerRadius: 5)
-                                .frame(width: 92, height: 98)
-                                .foregroundColor(.white)
-                                .shadow(color: Color.primaryShadowGray, radius: 2, x: 0, y: 0)
-                                .overlay(
-                                    VStack(spacing: 0) {
-                                        Text("Tall")
-                                            .font(.title2)
-                                        Text("355ml")
-                                            .padding(.top, 12)
-                                            .font(.caption)
-                                            .foregroundColor(.secondaryTextGray)
-                                    })
-                                .foregroundColor(.customBlack)
-                                .onTapGesture {
-                                    print("Tall 누름")
-                                }
-                        }
-                        .padding(.top, 38)
-                        .padding(.bottom, 6)
-                        Group {
-                            HStack {
-                                Text("샷 수")
-                                    .font(.title3)
-                                    .foregroundColor(.customBlack)
-                                Spacer()
-                                Text("2샷")
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryBrown)
-                            }
-                            .padding(.top, 40)
-                            HStack {
-                                Text("카페인")
-                                    .font(.title3)
-                                    .foregroundColor(.customBlack)
-                                Spacer()
-                                Text("175mg")
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryBrown)
-                            }
-                            .padding(.top, 40)
-                            HStack {
-                                Spacer()
-                                Text("하루 권장량 400mg")
-                                    .padding(.top, 5)
-                                    .font(.caption)
-                                    .foregroundColor(.secondaryTextGray)
-                            }
-                            HStack {
-                                Text("배출")
-                                    .font(.title3)
-                                    .foregroundColor(.customBlack)
-                                Spacer()
-                                Text("2시간 45분")
-                                    .font(.title3)
-                                    .fontWeight(.semibold)
-                                    .foregroundColor(.primaryBrown)
-                            }
-                            .padding(.top, 19)
-                            .padding(.bottom, 38)
-                            
-                        }
-                        .padding(.horizontal, 19)
+                        CaffeineBeverage()
+                        CaffeineSizeButtonGroup()
+                        CaffeineInfoGroup()
                     }
-                    .background(Color.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 10))
-                    .padding(.horizontal, 15)
-                    .padding(.top, 15)
-                    .shadow(color: Color.primaryShadowGray, radius: 4, x: 0, y: 0)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: CaffeineBookDetailLayoutValue.Radius.backgroundCard))
+                    .padding(.horizontal, CaffeineBookDetailLayoutValue.Padding.backgroundHorizantal)
+                    .padding(.top, CaffeineBookDetailLayoutValue.Padding.backgroundTop)
+                    .shadow(color: Color.primaryShadowGray, radius: CaffeineBookDetailLayoutValue.Radius.backgroundCardShadow, x: .zero, y: .zero)
                     
                     Spacer()
                 }
@@ -152,6 +75,100 @@ struct CaffeineBookDetailView: View {
     }
 }
 
+struct CaffeineBeverage: View {
+    var body: some View {
+        Text("아메리카노")
+            .font(.title)
+            .fontWeight(.bold)
+            .foregroundColor(.customBlack)
+            .padding(.top, CaffeineBookDetailLayoutValue.Padding.titleTop)
+        
+        Text("스타벅스")
+            .font(.caption)
+            .foregroundColor(.secondaryTextGray)
+            .padding(.top, CaffeineBookDetailLayoutValue.Padding.franchiseTop)
+    }
+}
+
+struct CaffeineSizeButtonGroup: View {
+    var body: some View {
+        HStack(spacing: CaffeineBookDetailLayoutValue.Padding.buttonBetweenSpace) {
+            CaffeineSizeButton(size: "Tall", volume: "355ml")
+            CaffeineSizeButton(size: "Grande", volume: "450ml")
+            CaffeineSizeButton(size: "Venti", volume: "530ml")
+        }
+        .padding(.top, CaffeineBookDetailLayoutValue.Padding.buttonGroupTop)
+        .padding(.bottom, CaffeineBookDetailLayoutValue.Padding.buttonGroupBottom)
+    }
+}
+
+struct CaffeineSizeButton: View {
+    
+    var size: String
+    var volume: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: CaffeineBookDetailLayoutValue.Radius.sizeButton)
+            .frame(width: CaffeineBookDetailLayoutValue.Size.sizeButtonWidth, height: CaffeineBookDetailLayoutValue.Size.sizeButtonHeight)
+            .foregroundColor(.white)
+            .shadow(color: Color.primaryShadowGray, radius: CaffeineBookDetailLayoutValue.Radius.sizeButtonShadow, x: .zero, y: .zero)
+            .overlay(
+                VStack(spacing: .zero) {
+                    Text(size)
+                        .font(.title2)
+                        .foregroundColor(.customBlack)
+                    Text(volume)
+                        .padding(.top, CaffeineBookDetailLayoutValue.Padding.buttonTextSpace)
+                        .font(.caption)
+                        .foregroundColor(.secondaryTextGray)
+                }
+            )
+            .onTapGesture {
+                print("\(size) 누름")
+            }
+    }
+}
+
+struct CaffeineInfoGroup: View {
+    
+    var body: some View {
+        Group {
+            CaffeineInfoRow(label: "샷 수", info: "2샷")
+                .padding(.top, CaffeineBookDetailLayoutValue.Padding.infoRowBetweenSpaceExceptCaption)
+            CaffeineInfoRow(label: "카페인", info: "175mg")
+                .padding(.top, CaffeineBookDetailLayoutValue.Padding.infoRowBetweenSpaceExceptCaption)
+            HStack {
+                Spacer()
+                Text("하루 권장량 400mg")
+                    .padding(.top, CaffeineBookDetailLayoutValue.Padding.recommendedAmountTop)
+                    .font(.caption)
+                    .foregroundColor(.secondaryTextGray)
+            }
+            CaffeineInfoRow(label: "배출", info: "2시간 45분")
+                .padding(.bottom, CaffeineBookDetailLayoutValue.Padding.infoRowBottom)
+        }
+        .padding(.horizontal, CaffeineBookDetailLayoutValue.Padding.infoGroupHorizontal)
+    }
+}
+
+struct CaffeineInfoRow: View {
+    
+    var label: String
+    var info: String
+    
+    var body: some View {
+        HStack {
+            Text(label)
+                .foregroundColor(.customBlack)
+            Spacer()
+            Text(info)
+                .fontWeight(.semibold)
+                .foregroundColor(.primaryBrown)
+        }
+        .padding(.top, CaffeineBookDetailLayoutValue.Padding.infoRowBetweenSpaceCaption)
+        .font(.title3)
+    }
+}
 
 struct CaffeineBookDetailView_Previews: PreviewProvider {
     static var previews: some View {
