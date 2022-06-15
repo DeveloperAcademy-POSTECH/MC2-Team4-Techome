f74c8f0d909aaf0c9cb8f48b08ac70c3ee77c53b
feldblume5263 <feldblume5263@gmail.com>
2022-06-15 17:21:56 +0900

    update poding changes

---
+++ Techome/Techome/ViewModel/CaffeineBook/CaffeineBookStateHolder.swift
@@ -0,0 +1,22 @@
+//
+//  CaffeineBookStateHolder.swift
+//  Techome
+//
+//  Created by Yeongwoo Kim on 2022/06/14.
+//
+
+import Foundation
+
+final class CaffenineBookStateHolder: ObservableObject {
+    @Published var selectedCategory: Category = .franchise
+    @Published var selectedFranchise: Franchise = .starbucks
+    
+    let categories = Category.allCases
+    let franchises = Franchise.allCases
+}
+
+enum Category: String, CaseIterable {
+    case franchise = "프렌차이즈"
+    case personal = "홈/개인카페"
+    case mart = "마트/편의점"
+}
---
+++ Techome/Techome/Views/CaffeineBook/CaffeineBookView.swift
@@ -0,0 +1,40 @@
+//
+//  CaffeineBookView.swift
+//  Techome
+//
+//  Created by Yeongwoo Kim on 2022/06/14.
+//
+
+import SwiftUI
+
+struct CaffeineBookView: View {
+    @StateObject var stateHolder = CaffenineBookStateHolder()
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: .zero) {
+                CategoryRow()
+                    .environmentObject(stateHolder)
+                
+                if stateHolder.selectedCategory == .franchise {
+                    FranchiseRow()
+                        .environmentObject(stateHolder)
+                }
+                
+                BeverageList()
+                    .frame(width: screenWidth)
+                    .background(Color.backgroundCream)
+                    .environmentObject(stateHolder)
+            }
+            .navigationTitle("카페인 찾아보기")
+        }
+        .preferredColorScheme(.light)
+    }
+}
+
+struct CaffeineBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        CaffeineBookView()
+            .environmentObject(CaffenineBookStateHolder())
+    }
+}
