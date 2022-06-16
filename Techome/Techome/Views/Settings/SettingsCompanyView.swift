4506ce4eced3124775b9120e9d9353db05575dd2
Guel-git <eccyuna@gmail.com>
2022-06-16 10:28:55 +0900

    [feat] #48 SettingsCompanyView 코드 모듈화 - 추후에 layout value 추가 예정

--- Techome/Techome/Views/Settings/SettingsCompanyView.swift
+++ Techome/Techome/Views/Settings/SettingsCompanyView.swift
@@ -18,48 +18,9 @@ struct SettingsCompanyView: View {
                 
                 VStack(spacing: .zero) {
                     VStack(alignment: .center, spacing: .zero) {
-                        HStack(spacing: 0) {
-                            Text("택이네")
-                                .font(.title3)
-                                .foregroundColor(.customBlack)
-                            Spacer()
-                            Image("CaffeineBookIcon")
-                            
-                        }
-                        .padding(.vertical, 32)
-                        .padding(.horizontal, 17)
-                        
-                        VStack(alignment: .leading, spacing: .zero) {
-                            HStack(spacing: 0) {
-                                Text("구성원")
-                                    .font(.body)
-                                    .foregroundColor(.primaryBrown)
-                                Spacer()
-                            }
-                            
-                            Text("놔스닥(박준홍), 닉(김승윤), 메리(김휘원), 진저(김유나), 택(한택환), 포딩(김영우)")
-                                .padding(.top, 7)
-                            
-                            HStack(spacing: 0) {
-                                Text("팀 미션")
-                                    .font(.body)
-                                    .foregroundColor(.primaryBrown)
-                                Spacer()
-                            }
-                            .padding(.top, 16)
-                            
-                            Text("'택이네'는 '커피'라는 공통 관심사를 가지고 모인 디벨로퍼들로 구성된 팀입니다.")
-                                .padding(.top, 5)
-                            
-                            Text("사람들이 커피를 포함한 카페인 음료/음식으로 보다 더 활기찬 일상을 만들 수 있도록 함께 머리 모아 고민하는 여정을 함께 해왔습니다.")
-                                .padding(.top, 5)
-                            
-                        }
-                        .padding(.horizontal, 17)
-                        .padding(.bottom, 32)
-                        .foregroundColor(.customBlack)
-                        
-                        
+                        TeamNameLogoRow()
+                        MemberGroup()
+                        MissionGroup()
                     }
                     .background(.white)
                     .clipShape(RoundedRectangle(cornerRadius: 7))
@@ -68,7 +29,7 @@ struct SettingsCompanyView: View {
                     .shadow(color: Color.primaryShadowGray, radius: 4, x: .zero, y: .zero)
                     .font(.body)
                     .foregroundColor(.customBlack)
-                    .tint(.primaryBrown)
+                    
                     Spacer()
                 }
                 .padding(.top, 38)
@@ -85,6 +46,60 @@ struct SettingsCompanyView: View {
                 }
             )
         }
+    }
+}
+
+struct TeamNameLogoRow: View {
+    var body: some View {
+        HStack(spacing: 0) {
+            Text("택이네")
+                .font(.title3)
+                .foregroundColor(.customBlack)
+            Spacer()
+            Image("CaffeineBookIcon")
+            
+        }
+        .padding(.vertical, 32)
+        .padding(.horizontal, 17)
+    }
+}
+
+struct MemberGroup: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            HStack(spacing: 0) {
+                Text("구성원")
+                    .font(.body)
+                    .foregroundColor(.primaryBrown)
+                Spacer()
+            }
+            
+            Text("놔스닥(박준홍), 닉(김승윤), 메리(김휘원), 진저(김유나), 택(한택환), 포딩(김영우)")
+                .padding(.top, 7)
+        }
+        .padding(.horizontal, 17)
+    }
+}
+
+struct MissionGroup: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: .zero) {
+            HStack(spacing: 0) {
+                Text("팀 미션")
+                    .font(.body)
+                    .foregroundColor(.primaryBrown)
+                Spacer()
+            }
+            .padding(.top, 18)
+            
+            Text("'택이네'는 '커피'라는 공통 관심사를 가지고 모인 디벨로퍼들로 구성된 팀입니다.")
+                .padding(.top, 7)
+            
+            Text("사람들이 커피를 포함한 카페인 음료/음식으로 보다 더 활기찬 일상을 만들 수 있도록 함께 머리 모아 고민하는 여정을 함께 해왔습니다.")
+                .padding(.top, 5)
+        }
+        .padding(.horizontal, 17)
+        .padding(.bottom, 32)
     }
 }
 
