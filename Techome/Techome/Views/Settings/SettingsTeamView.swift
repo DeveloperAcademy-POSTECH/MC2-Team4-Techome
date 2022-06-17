25a16774e5340cafc2992272c843acc84003e5d3
Guel-git <eccyuna@gmail.com>
2022-06-17 15:02:21 +0900

    [feat] #48 SettingsTeamView 코드 리팩토링 - layout value 선언 - 변수명, 변수 선언, 모듈화 체크

--- Techome/Techome/Views/Settings/SettingsTeamView.swift
+++ Techome/Techome/Views/Settings/SettingsTeamView.swift
@@ -7,6 +7,27 @@
 
 import SwiftUI
 
+struct SettingsTeamLayoutValue {
+    
+    struct Padding {
+        static let cardTop: CGFloat = 23
+        static let overallHorizontal: CGFloat = 15
+        static let teamNameHorizontal: CGFloat = 17
+        static let teamNameVertical: CGFloat = 25
+        static let memberGroupHorizontal: CGFloat = 17
+        static let memberGroupBottom: CGFloat = 9
+        static let memberRoleBetween: CGFloat = 5
+        static let roleTop: CGFloat = 2
+        static let mottoTop: CGFloat = 3
+        static let mottoBottom: CGFloat = 16
+    }
+    
+    struct Radius {
+        static let card: CGFloat = 7
+        static let cardShadow: CGFloat = 4
+    }
+}
+
 struct SettingsTeamView: View {
     
     @Environment(\.presentationMode) var presentation: Binding<PresentationMode>
@@ -17,21 +38,10 @@ struct SettingsTeamView: View {
                 Color.backgroundCream.edgesIgnoringSafeArea(.all)
                 
                 VStack(spacing: .zero) {
-                    VStack(alignment: .center, spacing: .zero) {
-                        TeamNameLogoRow()
-                        MemberGroup()
-                    }
-                    .background(.white)
-                    .clipShape(RoundedRectangle(cornerRadius: 7))
-                    .padding(.horizontal, 15)
-                    .padding(.top, 8)
-                    .shadow(color: Color.primaryShadowGray, radius: 4, x: .zero, y: .zero)
-                    .font(.body)
-                    .foregroundColor(.customBlack)
-                    
+                    Card()
                     Spacer()
                 }
-                .padding(.top, 15)
+                .padding(.top, SettingsTeamLayoutValue.Padding.cardTop)
             }
             .navigationTitle("팀 소개")
             .navigationBarTitleDisplayMode(.inline)
@@ -48,17 +58,30 @@ struct SettingsTeamView: View {
     }
 }
 
-struct TeamNameLogoRow: View {
+struct Card: View {
     var body: some View {
-        HStack(spacing: 0) {
+        VStack(alignment: .leading, spacing: .zero) {
+            TeamNameRow()
+            MemberGroup()
+        }
+        .background(.white)
+        .clipShape(RoundedRectangle(cornerRadius: SettingsTeamLayoutValue.Radius.card))
+        .padding(.horizontal, SettingsTeamLayoutValue.Padding.overallHorizontal)
+        .shadow(color: .primaryShadowGray, radius: SettingsTeamLayoutValue.Radius.cardShadow, x: .zero, y: .zero)
+        .foregroundColor(.customBlack)
+    }
+}
+
+struct TeamNameRow: View {
+    var body: some View {
+        HStack(spacing: .zero) {
             Text("택이네")
                 .font(.title3)
-                .foregroundColor(.customBlack)
             Spacer()
             Image("TeamLogo")
         }
-        .padding(.top, 25)
-        .padding(.horizontal, 17)
+        .padding(.horizontal, SettingsTeamLayoutValue.Padding.teamNameHorizontal)
+        .padding(.vertical, SettingsTeamLayoutValue.Padding.teamNameVertical)
     }
 }
 
@@ -66,32 +89,33 @@ struct MemberGroup: View {
     
     private let members: [String] = ["Ginger", "Mary", "Nick", "Noasdaq", "Poding", "Taek"]
     private let roles: [String] = ["택이네 폭주족", "택이네 평화지킴이", "택이네 활력소", "택이네 버팀목", "택이네 과속측정기", "택이네 길잡이"]
-    private let motto: [String] = ["뭐든 미친듯이! 한 번 시작하면 끝까지 물고 늘어집니다.", "다양한 것에서 다양한 재미를 느끼며, 나에게 온 모든 일을 즐깁니다.", "현재를 만끽하자! 매 순간을 진심으로 대하고 열심히 나아갑니다.", "오늘은 어제보다 행복하기 위해 노력합니다.", "대체 불가능한 디벨로퍼가 되자!", "WWDC 2022 Winner"]
-                                              
+    private let mottos: [String] = ["뭐든 미친듯이! 한 번 시작하면 끝까지 물고 늘어집니다.", "다양한 것에서 다양한 재미를 느끼며, 나에게 온 모든 일을 즐깁니다.", "현재를 만끽하자! 매 순간을 진심으로 대하고 열심히 나아갑니다.", "오늘은 어제보다 행복하기 위해 노력합니다.", "대체 불가능한 디벨로퍼가 되자!", "WWDC 2022 Winner"]
+    
     var body: some View {
         VStack(alignment: .leading, spacing: .zero) {
             ForEach(0..<members.count, id: \.self) { member in
                 HStack(spacing: .zero) {
                     Text(members[member])
                         .font(.subheadline)
-                        .foregroundColor(.customBlack)
+                    
                     Text(roles[member])
                         .font(.caption)
                         .foregroundColor(.secondaryTextGray)
-                        .padding(.leading, 5)
-                        .padding(.top, 2)
+                        .padding(.leading, SettingsTeamLayoutValue.Padding.memberRoleBetween)
+                        .padding(.top, SettingsTeamLayoutValue.Padding.roleTop)
+                    
                     Spacer()
                 }
-                .padding(.top, 16)
                 
-                Text(motto[member])
+                Text(mottos[member])
                     .font(.caption)
                     .foregroundColor(.primaryBrown)
-                    .padding(.top, 3)
+                    .padding(.top, SettingsTeamLayoutValue.Padding.mottoTop)
+                    .padding(.bottom, SettingsTeamLayoutValue.Padding.mottoBottom)
             }
         }
-        .padding(.horizontal, 17)
-        .padding(.bottom, 25)
+        .padding(.horizontal, SettingsTeamLayoutValue.Padding.memberGroupHorizontal)
+        .padding(.bottom, SettingsTeamLayoutValue.Padding.memberGroupBottom)
     }
 }
 
