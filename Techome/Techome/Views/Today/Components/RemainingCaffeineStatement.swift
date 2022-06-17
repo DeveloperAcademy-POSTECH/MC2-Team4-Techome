e03c049752a345f853d8382aa24ef3ed7cfebe2c
feldblume5263 <feldblume5263@gmail.com>
2022-06-17 14:47:06 +0900

    [feat] #47 merge 오류로 인한 재 업로드
    commit 내역은 todayView_q 를 참조해주세요...

---
+++ Techome/Techome/Views/Today/Components/RemainingCaffeineStatement.swift
@@ -0,0 +1,39 @@
+//
+//  RemainingCaffeineStatement.swift
+//  Techome
+//
+//  Created by Noah Park on 2022/06/15.
+//
+
+import SwiftUI
+
+struct RemainingCaffeineStatement: View {
+    @ObservedObject var todayStates: TodayStatesHolder
+    
+    var body: some View {
+        VStack(spacing: .zero) {
+            HStack(alignment: .firstTextBaseline, spacing: .zero) {
+                Text("몸속에")
+                    .font(.title3)
+                    .padding(.trailing, TodayLayoutValue.Padding.RemainingStatement.textVInterval)
+                Text("\(Int(todayStates.remainingAmount))")
+                    .font(.system(size: TodayLayoutValue.Size.remainingTextSize).bold())
+                    .foregroundColor(.primaryBrown)
+                Text("mg의")
+                    .font(.title3)
+            }
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            Text("카페인이 남아있고")
+                .font(.title3)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+        }
+    }
+}
+
+struct RemainingCaffeineStatement_Previews: PreviewProvider {
+    static var previews: some View {
+        let todayStates: TodayStatesHolder = TodayStatesHolder()
+        RemainingCaffeineStatement(todayStates: todayStates)
+            .previewLayout(.sizeThatFits)
+    }
+}
