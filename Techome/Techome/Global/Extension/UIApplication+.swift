380ef036b01860c10e660918d3c60bfacd2b319e
feldblume5263 <feldblume5263@gmail.com>
2022-06-20 09:30:47 +0900

    update back gesture

--- Techome/Techome/Global/Extension/UIApplication+.swift
+++ Techome/Techome/Global/Extension/UIApplication+.swift
@@ -15,3 +15,14 @@ extension UIApplication {
     }
     
 }
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
